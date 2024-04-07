d44ac5f1fab4205b88ab2f5459b2a9c8eab517dd
Stephen Cheng <zintaen@gmail.com>
2024-04-08 01:42:53 +0700

    init project

---
+++ lib/features/product/domain/repositories/product_repository_interface.dart
@@ -0,0 +1,13 @@
+
+import 'package:sixvalley_vendor_app/data/model/response/base/api_response.dart';
+import 'package:sixvalley_vendor_app/interface/repository_interface.dart';
+
+abstract class ProductRepositoryInterface implements RepositoryInterface{
+  Future<ApiResponse> getSellerProductList(String sellerId, int offset, String languageCode, String search );
+  Future<ApiResponse> getPosProductList(int offset, List <String> ids);
+  Future<ApiResponse> getSearchedPosProductList(String search, List <String> ids);
+  Future<ApiResponse> getStockLimitedProductList(int offset, String languageCode );
+  Future<ApiResponse> getMostPopularProductList(int offset, String languageCode );
+  Future<ApiResponse> getTopSellingProductList(int offset, String languageCode );
+
+}
